ad0ae5b3fd1975d02ecd966e00be54186ae8f23a
Chivy221 <danilius227@gmail.com>
2025-06-08 16:19:19 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-
-const MessageSchema = new mongoose.Schema({
-sender: String,
-recipient: String,
-content: String, // encrypted
-createdAt: {
-type: Date,
-default: Date.now
-}
+const messageSchema = new mongoose.Schema({
+from: String,
+to: String,
+content: String,
+timestamp: { type: Date, default: Date.now },
 });
+module.exports = mongoose.model('Message', messageSchema);
