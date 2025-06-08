8f6ddcead3aaa0cfa2b7b9dda054ec21f5f9f9bb
Chivy221 <danilius227@gmail.com>
2025-06-08 15:17:45 +0300

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const logFile = path.join(__dirname, '..', 'logs.txt');
+
+function log(message) {
+const logLine = [${new Date().toISOString()}] ${message}\n;
+fs.appendFile(logFile, logLine, err => {
+if (err) console.error('Log write error', err);
+});
+}
+
 module.exports = {
-info: (...args) => console.log('[INFO]', ...args),
-error: (...args) => console.error('[ERROR]', ...args)
+info: log,
+error: log
 };
