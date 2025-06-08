1a3eab6702c797877de4bb00164b43906196f7ec
Chivy221 <danilius227@gmail.com>
2025-06-08 15:06:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-require('dotenv').config();
+const dotenv = require('dotenv');
+const messagesRouter = require('./routes/messages');
+const logger = require('./utils/logger');
+const consumer = require('./rabbitmq/consumer');
 
-const messageRoutes = require('./routes/messages');
+dotenv.config();
+
+const app = express();
 app.use(express.json());
-app.use('/messages', messageRoutes);
+app.use('/messages', messagesRouter);
+
 app.get('/ping', (_, res) => res.send('pong'));
 
+// Simple /metrics endpoint
+app.get('/metrics', (_, res) => {
+res.json({
+uptime: process.uptime(),
+message: "OK",
+timestamp: Date.now()
+});
+});
+
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+useNewUrlParser: true,
+useUnifiedTopology: true
 }).then(() => {
-  console.log('Message DB connected');
-  app.listen(3000, () => console.log('Message service on port 3000'));
-}).catch(err => console.error(err));
+logger.info('Connected to MongoDB');
+app.listen(process.env.PORT, () =>
+logger.info(Messages service running on port ${process.env.PORT})
+);
+consumer(); // Start RabbitMQ consumer
+}).catch(err => logger.error('MongoDB connection error', err));
