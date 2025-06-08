1ab4d8d32c026a88f89b2ac12d4b627a984afc35
Chivy221 <danilius227@gmail.com>
2025-06-08 15:16:23 +0300

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message');
-const { encrypt, decrypt } = require('../utils/crypto');
+const { encrypt, decrypt } = require('../utils/encryption');
 const cache = require('../utils/cache');
+const logger = require('../utils/logger');
 
-router.post('/', async (req, res) => {
-try {
-const { sender, recipient, content } = req.body;
-const message = new Message({
-sender,
-recipient,
-content: encrypt(content)
-});
-await message.save();
-cache.del('messages');
-res.status(201).json({ ...message.toObject(), content });
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
-});
-
-router.get('/', async (_, res) => {
-try {
+// GET /messages
+router.get('/', async (req, res) => {
 const cached = cache.get('messages');
-if (cached) return res.json(cached);
+if (cached) {
+logger.info('Cache hit: /messages');
+return res.json(cached);
+}
+
 const messages = await Message.find();
 const decrypted = messages.map(msg => ({
 ...msg.toObject(),
 content: decrypt(msg.content)
 }));
+
 cache.set('messages', decrypted);
+logger.info('Fetched messages from DB');
 res.json(decrypted);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+});
+
+// POST /messages
+router.post('/', async (req, res) => {
+const { sender, receiver, content } = req.body;
+const encryptedContent = encrypt(content);
+
+const newMessage = new Message({ sender, receiver, content: encryptedContent });
+await newMessage.save();
+logger.info(New message created from ${sender} to ${receiver});
+cache.del('messages');
+res.status(201).json({ ...newMessage.toObject(), content });
 });
 
 module.exports = router;
