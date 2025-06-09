0c5dabdef728e0e585e207a8450682277f5e7df4
Chivy221 <danilius227@gmail.com>
2025-06-09 14:48:35 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,12 @@
+const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const MessageSchema = new mongoose.Schema({
-  id: { type: String, default: uuidv4 }, // добавляем shard key
+  id: { type: String, default: uuidv4 }, // shard key
   from: String,
   to: String,
   content: String,
   createdAt: { type: Date, default: Date.now },
 });
+
+module.exports = mongoose.model('Message', MessageSchema);
